de89ef7fa90c8ec41f08afe233c8262a29e55e6e
bpjfarias <bpjfarias@gmail.com>
2021-01-27 17:44:49 -0300

    adding lint,  head, form and button and route quiz

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from './src/components/Widget'
-import Footer from './src/components/Footer'
-import GitHubCorner from './src/components/GitHubCorner'
-import QuizBackground from './src/components/QuizBackground'
+/* eslint-disable linebreak-style */
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import React from 'react';
+import styled from 'styled-components';
+import db from '../db.json';
+import Footer from './src/components/Footer';
+import GitHubCorner from './src/components/GitHubCorner';
+import QuizBackground from './src/components/QuizBackground';
+import Widget from './src/components/Widget';
 
 const QuizContainer = styled.div`
   width: 100%;
@@ -17,36 +21,54 @@ const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Tibians Quiz</title>
+      </Head>
       <QuizContainer>
-        < Widget>   
+        <Widget>
 
-            <Widget.Header>
+          <Widget.Header>
             <h1>{db.title}</h1>
-            </Widget.Header>
+          </Widget.Header>
 
-            <Widget.Content>
+          <Widget.Content>
             <p>{db.description}</p>
+            <form onSubmit={function (event) {
+              event.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <input
+                placeholder="Qual seu nome?"
+                onChange={function (event) {
+                  setName(event.target.value);
+                }}
+              />
+              <button type="submit" disabled={!name}>
+                Boa sorte, {name}
+              </button>
+            </form>
           </Widget.Content>
-          
         </Widget>
 
-        < Widget>    
-
-            <Widget.Header>
+        <Widget>
+          <Widget.Header>
             <h1>Quizes da galera</h1>
-            </Widget.Header>
+          </Widget.Header>
 
-            <Widget.Content>
+          <Widget.Content>
             <p>Em breve aqui aparecerão os quizes de quem também fez a semana da Alura!</p>
           </Widget.Content>
 
         </Widget>
         <Footer />
       </QuizContainer>
-    <GitHubCorner projectUrl="https://github.com/bpjfarias"/>
+      <GitHubCorner projectUrl="https://github.com/bpjfarias" />
     </QuizBackground>
-
-  )
+  );
 }
